32241d9de3c93343cdd566189a1b5f17e002df0d
Sergio Belalcazar <s.a.belalcazar.c@gmail.com>
2023-01-09 20:28:15 -0500

    added working quiz

---
+++ script.js
@@ -0,0 +1,121 @@
+// questions
+
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "javascript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+
+//consts
+
+const quiz= document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit")
+const viewScores = document.getElementById("scores")
+
+
+let currentQuiz = 0
+let score = 0
+
+
+loadQuiz()
+
+function timer(){
+    var sec = 60;
+    var timer = setInterval(function(){
+        document.getElementById("countdown").innerHTML=''+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+
+function loadQuiz() {
+    
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+    timer();
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEls => answerEls.checked = false);
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+//////////////////////
+// EVENT LISTENERS //
+//////////////////////
+
+
+
+
+submitBtn.addEventListener("click", () => {
+    const answer = getSelected()
+    if(answer) {
+        if (answer === quizData[currentQuiz].correct)
+            score++
+    }
+    currentQuiz++; 
+
+    if(currentQuiz < quizData.length) {
+        loadQuiz()
+    } else {
+        quiz.innerHTML = `
+        <h2>You answered ${score}/${quizData.length} questions correctly!</h2>
+        <p id="endMessage">Try again?</p>
+        <form id="signup">Enter your initials here</form>
+        <button id="reload" onclick="location.reload()">Reload</button>
+        
+        `
+    }
+}
+)
